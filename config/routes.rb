adaa96088bcdfef56f7012db54216346eb13933a
ccslakey <cromwellslakey@gmail.com>
2015-12-12 13:51:23 -0800

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  
   get 'signup'  => 'users#new'  
-
+  resources :users
 end
