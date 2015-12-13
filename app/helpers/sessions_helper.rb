faea94308f5e0598a27ee8a17e20a51506705086
ccslakey <cromwellslakey@gmail.com>
2015-12-13 13:02:22 -0800

    finish user CRUD actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,6 +17,11 @@ module SessionsHelper
       end
     end
 
+    # Returns true if the given user is the current user.
+    def current_user?(user)
+      user == current_user
+    end
+
     # Returns true if the user is logged in, false otherwise.
     def logged_in?
       !current_user.nil?
@@ -39,5 +44,16 @@ module SessionsHelper
       user.remember
       cookies.permanent.signed[:user_id] = user.id
       cookies.permanent[:remember_token] = user.remember_token
+    end
+
+    # Redirects to stored location (or to the default).
+    def redirect_back_or(default)
+      redirect_to(session[:forwarding_url] || default)
+      session.delete(:forwarding_url)
+    end
+
+    # Stores the URL trying to be accessed.
+    def store_location
+      session[:forwarding_url] = request.url if request.get?
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,23 +3,25 @@ class User < ActiveRecord::Base
 
   before_save {self.email = email.downcase}
   validates :name,  presence: true, length: { maximum: 50 }
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, 
+                       length: { minimum: 6 }, allow_nil: true
 
   # Returns the hash digest of the given string.
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
   # Returns a random token.
-  def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
 
