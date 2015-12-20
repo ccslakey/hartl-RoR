6c22a46594b96824c97fde80bebd23fd6882ea19
ccslakey <cromwellslakey@gmail.com>
2015-12-19 17:34:19 -0800

    ch 11.4.2 or listing 11.60. kicked off image uploading

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,9 +1,9 @@
 class Micropost < ActiveRecord::Base
-  belongs_to :user
-  validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }
-
-  default_scope -> { order(created_at: :desc) }
-
+  
+    belongs_to :user
+    default_scope -> { order(created_at: :desc) }
+    mount_uploader :picture, PictureUploader
+    validates :user_id, presence: true
+    validates :content, presence: true, length: { maximum: 140 }
 
 end
